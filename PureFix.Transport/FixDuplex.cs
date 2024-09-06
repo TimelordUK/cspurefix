e5e2fe6d737559b544df906c80dd34a5f0aab1ee
timelorduk <sjames8888@gmail.com>
2024-09-06 13:18:25 +0100

    add a duplex currently a channel which parser uses to publish parsed messages.

---
+++ PureFix.Transport/FixDuplex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace PureFix.Transport
+{
+    public class FixDuplex<T>
+    {
+        private readonly Channel<T> _channel = Channel.CreateUnbounded<T>();
+        public ChannelReader<T> Reader => _channel.Reader;
+        public ChannelWriter<T> Writer => _channel.Writer;
+    }
+}
