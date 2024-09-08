c460daeac7d01a849c5023e6e9ca6f45278346c8
timelorduk <sjames8888@gmail.com>
2024-09-08 14:24:28 +0100

    ascii view parsing to types

---
+++ PureFix.Buffer/ElasticBuffer.DateTime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using PureFix.Buffer.Ascii;
+
+namespace PureFix.Buffer
+{
+    public partial class ElasticBuffer
+    {
+        public char[] GetChars(int st, int vend)
+        {
+            var span = new ReadOnlySpan<byte>(_buffer, st, vend - st + 1);
+            var chars = new char[span.Length];
+            var itr = span.GetEnumerator();
+            var j = 0;
+            while (itr.MoveNext())
+            {
+                chars[j++] = (char)itr.Current;
+            }
+            return chars;
+        }
+
+        // 10:39:01.621
+        public DateTime? GetLocalTime(int st, int vend)
+        {
+            var chars = GetChars(st, vend);
+            if (DateTime.TryParseExact(chars, "hh:mm:ss.fff".AsSpan(), null,DateTimeStyles.AssumeLocal, out var d))
+            {
+                return d;
+            }
+            if (DateTime.TryParseExact(chars, "hh:mm:ss".AsSpan(), null, DateTimeStyles.AssumeLocal, out var d1))
+            {
+                return d1;
+            }
+            return null;
+        }
+    }
+}
