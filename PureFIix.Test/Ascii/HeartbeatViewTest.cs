cd1f44caa513b164a76dbef31ce0f0741d336073
TimelordUK <sjames8888@gmail.com>
2024-09-09 21:21:39 +0100

    hand build test messages as if generated by compiler

---
+++ PureFIix.Test/Ascii/HeartbeatViewTest.cs
@@ -0,0 +1,51 @@
+﻿using PureFIix.Test.Env;
+using PureFix.Buffer.Ascii;
+using PureFix.ParserFormat;
+using PureFix.Types.FIX4._4.quickfix;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureFIix.Test.Ascii
+{
+    internal class HeartbeatMsgViewTest
+    {
+        private TestEntity _testEntity;
+        private List<AsciiView> _views;
+
+        [OneTimeSetUp]
+        public async Task OnceSetup()
+        {
+            _testEntity = new TestEntity();
+            _views = await _testEntity.Replay(Fix44PathHelper.HeartbeatReplayPath);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _testEntity.Prepare();
+        }
+
+        [Test]
+        public void Get_View_Test()
+        {
+            Assert.That(_views, Is.Not.Null);
+            Assert.That(_views, Has.Count.EqualTo(1));
+            var mv = _views[0];
+            Assert.That(mv, Is.Not.Null);
+        }
+
+        [Test]
+        public void Parse_Header_View_Test()
+        {
+            Assert.That(_views, Is.Not.Null);
+            Assert.That(_views, Has.Count.EqualTo(1));
+            var mv = _views[0];
+            Assert.That(mv, Is.Not.Null);
+            var hb = new Heartbeat();
+            hb.Parse(mv);
+        }
+    }
+}
---
+++ PureFix.Types/FIX4.4/quickfix/set/StandardTrailer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureFix.Types.FIX4._4.quickfix.set
+{
+    public class StandardTrailer
+    {
+        public int? SignatureLength { get; set; } // [1] 93 (Length)
+        public byte[]? Signature { get; set; } // [2] 89 (RawData)
+        public string? CheckSum { get; set; } // [3] 10 (String)
+    }
+}
