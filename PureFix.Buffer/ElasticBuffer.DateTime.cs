127c3a63bcd93a44e840c7a34d6598f20b251ec9
timelorduk <sjames8888@gmail.com>
2024-09-09 12:51:33 +0100

    start date and time format parsing

--- PureFix.Buffer/ElasticBuffer.DateTime.cs
+++ PureFix.Buffer/ElasticBuffer.DateTime.cs
@@ -13,17 +13,58 @@ using PureFix.Buffer.Ascii;
 
 namespace PureFix.Buffer
 {
+    /*
+     * case "utctimestamp":
+       {
+           return TagType.UtcTimestamp;
+       }
+
+       case "localmktdate":
+       {
+           return TagType.LocalDate;
+       }
+
+       case "utcdateonly":
+       {
+           return TagType.UtcDateOnly;
+       }
+
+       case "utctimeonly":
+       {
+           return TagType.UtcTimeOnly;
+       }
+     */
     public partial class ElasticBuffer
     {
         // 10:39:01.621
         public DateTime? GetLocalTime(int st, int vend)
         {
-            return GetTime(st, vend, DateTimeStyles.AssumeLocal);
+            var v = GetTime(st, vend, DateTimeStyles.AssumeLocal);
+            if (v == null) return v;
+            return v.Value.ToLocalTime();
         }
 
-        public DateTime? GetUtcTime(int st, int vend)
+        // case "utctimeonly":
+        public DateTime? GetUtcTimeOnly(int st, int vend)
         {
-            return GetLocalTime(st, vend)?.ToUniversalTime();
+            var v = GetTime(st, vend, DateTimeStyles.AssumeUniversal);
+            if (v == null) return v;
+            return v.Value.ToUniversalTime();
+        }
+
+        public int WriteUtcTimeOnly(DateTime dateTime)
+        {
+            return WriteLocalTimeOnly(dateTime.ToUniversalTime());
+        }
+
+        public int WriteLocalTimeOnly(DateTime dateTime)
+        {
+            var format = "HH:mm:ss.fff".AsSpan();
+            CheckGrowBuffer(format.Length);
+            var span = _buffer.AsSpan()[Pos..format.Length];
+            dateTime.TryFormat(span, out var written, format);
+            Pos += written;
+            return Pos;
         }
 
         private DateTime? GetTime(int st, int vend, DateTimeStyles style)
@@ -35,11 +76,11 @@ namespace PureFix.Buffer
             {
                 chars[j++] = (char)c;
             }
-            if (DateTime.TryParseExact(chars, "hh:mm:ss.fff".AsSpan(), null, style, out var d))
+            if (DateTime.TryParseExact(chars, "HH:mm:ss.fff".AsSpan(), null, style, out var d))
             {
                 return d;
             }
-            if (DateTime.TryParseExact(chars, "hh:mm:ss".AsSpan(), null, style, out var d1))
+            if (DateTime.TryParseExact(chars, "HH:mm:ss".AsSpan(), null, style, out var d1))
             {
                 return d1;
             }
