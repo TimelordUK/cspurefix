71d8ec6f400d8b98faa54af8689c470c55d7da2e
foxesknow <seanbatten58@gmail.com>
2024-09-12 10:44:19 +0100

    Added benchmark console

---
+++ PureFix.Benchmarks/Fix44PathHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureFix.Benchmarks
+{
+    public static class Fix44PathHelper
+    {
+        public static readonly string Root = Directory.GetCurrentDirectory();
+        public static readonly string Fix44 = Path.Join(Root, "Data", "FIX44.xml");
+        public static readonly string HeartbeatFile = Path.Join(Root, "Messages", "Heartbeat.txt");
+    }
+}
