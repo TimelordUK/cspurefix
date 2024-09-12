91e4fd8f70dd8eb6bc71fee6cf5f66680b9e39a6
foxesknow <seanbatten58@gmail.com>
2024-09-12 11:03:20 +0100

    Added logon and execution report messages to benchmark

--- PureFix.Benchmarks/Fix44PathHelper.cs
+++ PureFix.Benchmarks/Fix44PathHelper.cs
@@ -11,5 +11,7 @@ namespace PureFix.Benchmarks
         public static readonly string Root = Directory.GetCurrentDirectory();
         public static readonly string Fix44 = Path.Join(Root, "Data", "FIX44.xml");
         public static readonly string HeartbeatFile = Path.Join(Root, "Messages", "Heartbeat.txt");
+        public static readonly string LogonFile = Path.Join(Root, "Messages", "Logon.txt");
+        public static readonly string ExecutionReportFile = Path.Join(Root, "Messages", "ExecutionReport.txt");
     }
 }
---
+++ PureFix.Benchmarks/MessageParsingBenchmarks.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BenchmarkDotNet.Attributes;
+using CommandLine;
+using PureFix.Buffer.Ascii;
+using PureFix.Dictionary.Definition;
+using PureFix.Dictionary.Parser.QuickFix;
+
+namespace PureFix.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class MessageParsingBenchmarks
+    {
+        private const string DataDict = "FIX44.xml";
+
+        private readonly byte[] _HeartbeatMessage;
+        private readonly byte[] _LogonMessage;
+        private readonly byte[] _ExecutionReportMessage;
+
+        private readonly FixDefinitions _FixDefinitions;
+        private readonly AsciiParser _Parser;
+
+        private readonly Action<int, AsciiView> NoAction = (index, view) => {};
+
+        public MessageParsingBenchmarks()
+        {
+            _HeartbeatMessage = LoadMessage(Fix44PathHelper.HeartbeatFile);
+            _LogonMessage = LoadMessage(Fix44PathHelper.LogonFile);
+            _ExecutionReportMessage = LoadMessage(Fix44PathHelper.ExecutionReportFile);
+
+            _FixDefinitions = new FixDefinitions();
+            var qf = new QuickFixXmlFileParser(_FixDefinitions);
+            qf.Parse(Fix44PathHelper.Fix44);
+            
+            _Parser = new AsciiParser(_FixDefinitions) 
+            { 
+                Delimiter = AsciiChars.Pipe 
+            };
+        }
+
+        private byte[] LoadMessage(string filename)
+        {
+            var message = System.IO.File.ReadAllText(filename);
+            return System.Text.Encoding.UTF8.GetBytes(message);
+        }
+
+        [Benchmark]
+        public void Heartbeat()
+        {
+            _Parser.ParseFrom(_HeartbeatMessage, NoAction);
+        }
+
+        [Benchmark]
+        public void Logon()
+        {
+            _Parser.ParseFrom(_LogonMessage, NoAction);
+        }
+
+        [Benchmark]
+        public void ExecutionReport()
+        {
+            _Parser.ParseFrom(_ExecutionReportMessage, NoAction);
+        }
+    }
+}
