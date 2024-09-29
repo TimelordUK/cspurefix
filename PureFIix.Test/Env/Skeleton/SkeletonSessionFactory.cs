d44d52b43a4b8a021e9883a26332beb4d3dc231e
TimelordUK <sjames8888@gmail.com>
2024-09-30 00:07:33 +0100

    move basefactory

--- PureFIix.Test/Env/Skeleton/SkeletonSessionFactory.cs
+++ PureFIix.Test/Env/Skeleton/SkeletonSessionFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Arrow.Threading.Tasks;
+using PureFIix.Test.Env.Experiment;
 using PureFix.Buffer;
 using PureFix.Transport.Session;
 using PureFix.Transport.Store;
@@ -11,28 +12,7 @@ using System.Threading.Tasks;
 
 namespace PureFIix.Test.Env.Skeleton
 {
-    public abstract class BaseSessionFactory : ISessionFactory
-    {
-        protected readonly IFixConfig m_config;
-        protected readonly IFixMessageFactory m_fixMessageFactory;
-        protected readonly IMessageParser m_parser;
-        protected readonly IMessageEncoder m_encoder;
-        protected readonly AsyncWorkQueue m_q;
-        protected readonly IFixClock m_clock;
-        protected readonly IFixMsgStore m_msgStore;
-        protected BaseSessionFactory(IFixConfig config, IFixMessageFactory fixMessageFactory, IMessageParser parser, IMessageEncoder encoder, IFixMsgStore store, AsyncWorkQueue q, IFixClock clock)
-        {
-            m_config = config;
-            m_fixMessageFactory = fixMessageFactory;
-            m_parser = parser;
-            m_encoder = encoder;
-            m_q = q;
-            m_clock = clock;
-            m_msgStore = store;
-        }
-        public abstract FixSession MakeSession();
-    }
-
+  
     internal class SkeletonSessionFactory : BaseSessionFactory
     {
         public SkeletonSessionFactory(IFixConfig config, IFixMessageFactory fixMessageFactory, IMessageParser parser, IMessageEncoder encoder, IFixMsgStore store, AsyncWorkQueue q, IFixClock clock) : base(config, fixMessageFactory, parser, encoder, store, q, clock)
