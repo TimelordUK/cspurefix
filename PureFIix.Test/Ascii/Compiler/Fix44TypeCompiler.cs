6123319f861049566ccc73490309c5116fdd20eb
sjame <sjame@WINDOWS-S8NTN26>
2024-10-05 15:10:35 +0100

    add a fix file trimmer which truncates the fix xml down to only those messages and fields specified.

--- PureFIix.Test/Ascii/Compiler/Fix44TypeCompiler.cs
+++ PureFIix.Test/Ascii/Compiler/Fix44TypeCompiler.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PureFix.Dictionary.Compiler;
+using PureFix.Dictionary.Parser.QuickFix;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using PureFix.Dictionary.Definition;
 
 namespace PureFIix.Test.Ascii.Compiler
 {
@@ -29,6 +32,36 @@ namespace PureFIix.Test.Ascii.Compiler
         public void MessageGenerator()
         {
             var generator = new MessageGenerator(null, _testEntity.Definitions,Options.FromVersion(_testEntity.Definitions));
+            generator.Process();
+        }
+
+        private IFixDefinitions GetTrimDefinitions(string[] types) {
+            var builder = new QuickFixXmlFileBuilder(_testEntity.Definitions);
+            var encoded = builder.Write(types);
+            var defs = new FixDefinitions();
+            var parser = new QuickFixXmlFileParser(defs);
+            parser.ParseText(encoded);
+            return defs;
+        }
+
+        [Test]
+        public void Check_Builder()
+        {
+            string[] msgTypes = { "0", "1", "2", "3", "4", "5", "AE" };
+            var newdDefinitions = GetTrimDefinitions(msgTypes);
+            Assert.That(newdDefinitions, Is.Not.Null);
+            foreach (var type in msgTypes)
+            {
+                var m = newdDefinitions.Message.GetValueOrDefault(type);
+                Assert.That(m, Is.Not.Null);
+            }
+        }
+   
+        [Test]
+        public void FileGenerator()
+        {
+            
+            var generator = new MessageGenerator(null, _testEntity.Definitions, Options.FromVersion(_testEntity.Definitions));
             generator.Process();
         }
     }
