4a62e21b0d11eee264dbf6e757f50f45716130d3
sjame <sjame@WINDOWS-S8NTN26>
2024-10-07 16:13:48 +0100

    partial for main prog

--- PureFix.App/Program.cs
+++ PureFix.App/Program.cs
@@ -63,24 +63,6 @@ internal partial class Program
         }
     }
 
-    private static IFixDefinitions GetDefinitions(CommandOptions options)
-    {
-        var definitions = new FixDefinitions();
-        var qfParser = new QuickFixXmlFileParser(definitions);
-        qfParser.Parse(options.DictPath);
-        return definitions;
-    }
-
-    private static Options GetGeneratorOptions(IFixDefinitions definitions, CommandOptions options)
-    {
-        var generatorOptions = Options.FromVersion(definitions);
-        var name = Path.GetFileName(options.DictPath);
-        name = name.Replace(".xml", string.Empty).Replace(".", string.Empty);
-        generatorOptions.BackingTypeOutputPath = Path.Join(options.OutputPath, "PureFix.Types", name, "QuickFix");
-        generatorOptions.BackingTypeNamespace = Path.Join("PureFix.Types", name, "QuickFix");
-        return generatorOptions;
-    }
-
     private static void Generate(CommandOptions options)
     {
         if (string.IsNullOrEmpty(options.DictPath))
