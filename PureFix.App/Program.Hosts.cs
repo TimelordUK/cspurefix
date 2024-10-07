6ea909b8adfad71f92745f71f3709270123f7879
sjame <sjame@WINDOWS-S8NTN26>
2024-10-07 16:11:34 +0100

    split program into partial class

---
+++ PureFix.App/Program.Hosts.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Arrow.Threading.Tasks;
+using PureFix.Transport.Session;
+using PureFix.Types;
+
+namespace PureFix.ConsoleApp;
+
+internal partial class Program
+{
+    // have different makehosts based on application type
+    private static BaseAppDI MakeTradeCaptureAppHost(IFixClock clock, IFixConfig config)
+    {
+        var factory = new ConsoleLogFactory(config.Name());
+        var queue = new AsyncWorkQueue();
+        var app = new TradeCaptureDI(queue, factory, clock, config);
+        return app;
+    }
+
+    private static BaseAppDI MakeSkeletonAppHost(IFixClock clock, IFixConfig config)
+    {
+        var factory = new ConsoleLogFactory(config.Name());
+        var queue = new AsyncWorkQueue();
+        var app = new SkeletonDI(queue, factory, clock, config);
+        return app;
+    }
+}
--- PureFix.App/Program.cs
+++ PureFix.App/Program.cs
@@ -1,16 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Arrow.Threading.Tasks;
 using CommandLine;
 using PureFix.Dictionary.Compiler;
 using PureFix.Dictionary.Definition;
 using PureFix.Dictionary.Parser.QuickFix;
-using PureFix.Transport.Session;
-using PureFix.Types;
 
 namespace PureFix.ConsoleApp;
 
-internal class Program
+internal partial class Program
 {
     public static async Task Main(string[] args)
     {
@@ -39,33 +36,6 @@ internal class Program
         }
     }
 
-    private static void Examples()
-    {
-        Console.WriteLine("trim an input dictionary file and output only messages and dependent fields on given message set.");
-        Console.WriteLine("./PureFix.ConsoleApp -d FIX50SP2.xml -T 0 1 2 3 4 5 A AE");
-        Console.WriteLine("");
-        Console.WriteLine("tail a file and output as tag decoded resolving enums.");
-        Console.WriteLine("./PureFix.ConsoleApp -f logs/test_client-fix-log20241007.txt -d FIX50SP2.xml -t -o tags");
-        Console.WriteLine("");
-        Console.WriteLine("tail a file and output as json objects");
-        Console.WriteLine("./PureFix.ConsoleApp -f logs/test_client-fix-log20241007.txt -d FIX50SP2.xml -t -o json");
-        Console.WriteLine("");        
-        Console.WriteLine("parse a fix52 log in decode tag format with no tail");
-        Console.WriteLine("./PureFix.ConsoleApp -f logs/test_client-fix-log20241007.txt -d FIX50SP2.xml -o tags");
-        Console.WriteLine("");
-        Console.WriteLine("parse a fix44 log in json format with no tail");
-        Console.WriteLine("./PureFix.ConsoleApp -f logs/test_client-fix-log20241007.txt -d FIX44.xml -o json");
-        Console.WriteLine("");
-        Console.WriteLine("run built in skeleton client and server to logon and heartbeat");
-        Console.WriteLine("./PureFix.ConsoleApp -d FIX50SP2.xml -A test-qf52-acceptor.json -a sk");
-        Console.WriteLine("");
-        Console.WriteLine("run built in skeleton client and server to logon and heartbeat with fix44 config");
-        Console.WriteLine("./PureFix.ConsoleApp -d FIX44.xml -I test-qf44-initiator.json -A test-qf44-acceptor.json -a sk");
-        Console.WriteLine("");
-        Console.WriteLine("run built in trade capture client and server to logon and heartbeat");
-        Console.WriteLine("./PureFix.ConsoleApp -d FIX50SP2.xml -A test-qf52-acceptor.json -a tc");        
-    }
-
     private static void Trim(CommandOptions options)
     {
         if (string.IsNullOrEmpty(options.DictPath))
@@ -91,23 +61,6 @@ internal class Program
                 await Runner.Run(options, MakeTradeCaptureAppHost);
                 break;
         }
-    }
-
-    // have different makehosts based on application type
-    private static BaseAppDI MakeTradeCaptureAppHost(IFixClock clock, IFixConfig config)
-    {
-        var factory = new ConsoleLogFactory(config.Name());
-        var queue = new AsyncWorkQueue();
-        var app = new TradeCaptureDI(queue, factory, clock, config);
-        return app;
-    }
-
-    private static BaseAppDI MakeSkeletonAppHost(IFixClock clock, IFixConfig config)
-    {
-        var factory = new ConsoleLogFactory(config.Name());
-        var queue = new AsyncWorkQueue();
-        var app = new SkeletonDI(queue, factory, clock, config);
-        return app;
     }
 
     private static IFixDefinitions GetDefinitions(CommandOptions options)
