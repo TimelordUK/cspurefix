6bd2fd973c7f58ca59c99e0cbfb0fb97e279f0b1
stephen james <sjames8888@gmail.com>
2024-10-18 21:01:07 +0100

    change namespace

--- PureFix.Test/Types/MonthYearTests.cs
+++ PureFix.Test/Types/MonthYearTests.cs
@@ -1,14 +1,7 @@
-﻿using PureFix.Types;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
+using PureFix.Types;
 
-using NUnit.Framework;
-
-namespace PureFIix.Test.Types
+namespace PureFix.Test.Types
 {
     public class MonthYearTests
     {
